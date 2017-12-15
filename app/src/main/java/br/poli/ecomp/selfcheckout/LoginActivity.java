64c60653dad4c86de08461f8e03e272e09d9482d
Paula Pithon <paulapithon@gmail.com>
2017-12-15 10:01:56 -0300

    Adicionar tela de home

--- app/src/main/java/br/poli/ecomp/selfcheckout/LoginActivity.java
+++ app/src/main/java/br/poli/ecomp/selfcheckout/LoginActivity.java
@@ -1,13 +1,29 @@
 package br.poli.ecomp.selfcheckout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.email)
+    EditText mEmailEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    @OnClick(R.id.confirmar_login)
+    public void onConfirmar () {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+
+
 }
